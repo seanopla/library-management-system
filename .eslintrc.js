9daed39f747f476aef2d892fb9ece457e9fc2df5
sean <ahmadnaufalez@gmail.com>
2024-11-19 11:29:22 +0700

    removed prettuer eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
