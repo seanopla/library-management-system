a483ef99e273e7dff1be2c288c3992ca7fb448f4
sean <ahmadnaufalez@gmail.com>
2024-11-19 11:31:21 +0700

    added some route navigation

--- src/_nav.js
+++ src/_nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
-import { cilSpeedometer, cilStar } from '@coreui/icons'
+import { cilSpeedometer, cilPencil, cilBook, cilUser, cilLibraryBuilding } from '@coreui/icons'
 import { CNavGroup, CNavItem } from '@coreui/react'
 
 const _nav = [
@@ -12,9 +12,27 @@ const _nav = [
   },
   {
     component: CNavItem,
-    name: 'Login',
-    to: '/login',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    name: 'Members',
+    to: '/member',
+    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Librarian',
+    to: '/librarian',
+    icon: <CIcon icon={cilLibraryBuilding} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Books',
+    to: '/books',
+    icon: <CIcon icon={cilBook} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Transaction',
+    to: '/transaction',
+    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
   },
 ]
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
+import { exact } from 'prop-types'
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Member = React.lazy(() => import('./views/member/Member'))
+const Librarian = React.lazy(() => import('./views/librarian/Librarian'))
+const Transaction = React.lazy(() => import('./views/transaction/Transaction'))
+const Books = React.lazy(() => import('./views/books/Books'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/login', name: 'Login', element: Login },
+  { path: '/member', name: 'Member', element: Member, exact: true },
+  { path: '/librarian', name: 'Librarian', element: Librarian, exact: true },
+  { path: '/books', name: 'Books', element: Books, exact: true },
+  { path: '/transaction', name: 'Transaction', element: Transaction, exact: true },
 ]
 
 export default routes
