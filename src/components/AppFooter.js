dadd2fef129ad1f3fd97fad2af320780bf24f364
sean <ahmadnaufalez@gmail.com>
2024-11-19 11:32:09 +0700

    changed footer information

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,7 +4,7 @@ import { CFooter } from '@coreui/react'
 const AppFooter = () => {
   return (
     <CFooter className="px-4">
-      <div>
+      {/* <div>
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
           CoreUI
         </a>
@@ -15,7 +15,9 @@ const AppFooter = () => {
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
           CoreUI React Admin &amp; Dashboard Template
         </a>
-      </div>
+      </div> */}
+      <div className="">&copy; Library Management System</div>
+      <div className="ms-auto">Created By: Ahmad Naufal Fadhillah</div>
     </CFooter>
   )
 }
